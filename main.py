2884efc7ed2c9a53ef7d4b3c0a38aa6de83cc5fb
PieIsSpy <karldeejayomandac@gmail.com>
2024-03-20 20:42:23 +0800

    Test Commit

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
+import pygame, time
+
+pygame.init()
+window = pygame.display.set_mode((600, 300))
+gamefont = pygame.font.SysFont("Arial", 60)
+display = gamefont.render("Amogus Sus", 1, (255,255,0))
+window.blit(display, (100,100))
+
+run = True
+while run:
+    pygame.time.delay(100)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    pygame.display.update()
+
+pygame.quit()
+
+
 # This is a sample Python script.
 
 # Press Shift+F10 to execute it or replace it with your code.
